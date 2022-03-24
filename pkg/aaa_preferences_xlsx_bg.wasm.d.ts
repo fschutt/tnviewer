ef41a0805ae3930588a3569da76f235b5acbd46c
Felix Schütt <12084016+fschutt@users.noreply.github.com>
2022-03-24 10:48:43 +0100

    Update aaa_preferences_xlsx_bg.wasm.d.ts

--- pkg/aaa_preferences_xlsx_bg.wasm.d.ts
+++ pkg/aaa_preferences_xlsx_bg.wasm.d.ts
@@ -1,7 +1,7 @@
 /* tslint:disable */
 /* eslint-disable */
 export const memory: WebAssembly.Memory;
-export function xml_to_xlsx(a: number, b: number, c: number): void;
+export function xml_to_xlsx(a: number, b: number, c: number, d: number): void;
 export function __wbindgen_add_to_stack_pointer(a: number): number;
 export function __wbindgen_export_0(a: number): number;
 export function __wbindgen_export_1(a: number, b: number): void;
