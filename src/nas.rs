2c718063ed35ac70e44aea3221227878991ebd3b
Felix Schütt <felix.schuett@maps4print.com>
2024-07-04 07:56:34 +0200

    Fix NAS XML parsing

--- src/nas.rs
+++ src/nas.rs
@@ -5,41 +5,31 @@ use crate::xml::get_all_nodes_in_subtree;
 
 #[derive(Debug, Default, Clone, Serialize, Deserialize)]
 pub struct NasSVGFile {
-    pub objekte: BTreeMap<String, Shape>,
+    pub objekte: BTreeMap<String, TaggedPolygon>,
     pub crs: String,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub enum Shape {
-    Point {
-        location: SvgPoint,
-        attributes: BTreeMap<String, String>,
-    },
-    Line {
-        points: Vec<SvgPoint>,
-        attributes: BTreeMap<String, String>,
-    },
-    Polygon {
-        rings: Vec<SvgPolygon>,
-        attributes: BTreeMap<String, String>,
-    }
+#[derive(Debug, Default, Clone, Serialize, Deserialize)]
+pub struct TaggedPolygon {
+    pub poly: SvgPolygon,
+    pub attributes: BTreeMap<String, String>,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Default, Clone, PartialEq, Serialize, Deserialize)]
 pub struct SvgLine {
     pub points: Vec<SvgPoint>,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Default, Clone, Serialize, Deserialize)]
 pub struct SvgPolygon {
     pub outer_rings: Vec<SvgLine>,
     pub inner_rings: Vec<SvgLine>,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct SvgPoint {
-    pub x: f32,
-    pub y: f32,
+    pub x: f64,
+    pub y: f64,
 }
 
 /// Parse the XML, returns [AX_Gebauede => (Polygon)]
@@ -81,7 +71,8 @@ fn xml_nodes_to_nas_svg_file(xml: Vec<XmlNode>, whitelist: &[&str]) -> NasSVGFil
 
     // Objekte parsen
     let whitelist = std::collections::BTreeSet::from_iter(whitelist.iter().cloned());
-    let objekte_nodes = get_all_nodes_in_subtree(&xml, "wfs:member");
+    let objekte_nodes = get_all_nodes_in_subtree(&xml, "member");
+    let mut objekte = BTreeMap::new();
     for o in objekte_nodes.iter() {
         let o_node = match o.children.first() {
             Some(s) => s,
@@ -91,9 +82,78 @@ fn xml_nodes_to_nas_svg_file(xml: Vec<XmlNode>, whitelist: &[&str]) -> NasSVGFil
             continue;
         }
         let key = o_node.node_type.clone();
-    }
+        let patches = get_all_nodes_in_subtree(&o_node.children, "PolygonPatch");
+        if patches.is_empty() {
+            continue;
+        }
+        let mut outer_rings = Vec::new();
+        let mut inner_rings = Vec::new();
+        let children = patches.iter().flat_map(|s| s.children.clone()).collect::<Vec<_>>();
+        for e_i in children.iter() {
+            let external = match e_i.node_type.as_str() {
+                "exterior" => true,
+                "interior" => false,
+                _ => continue,
+            };
+            let linestrings = get_all_nodes_in_subtree(&e_i.children, "LineStringSegment");
+            let linestring_points = linestrings
+                .iter()
+                .flat_map(|s| {
+                    s.children.iter()
+                    .filter_map(|s| s.text.clone())
+                    .map(|text| {
+                        let pts = text
+                        .split_whitespace()
+                        .filter_map(|s| s.parse::<f64>().ok())
+                        .collect::<Vec<_>>();
+                        pts.chunks(2).filter_map(|f| {
+                            match f {
+                                [east, false_north] => Some(SvgPoint {
+                                    x: *east,
+                                    y: *false_north,
+                                }),
+                                _ => None,
+                            }
+                        }).collect::<Vec<_>>()
+                    })
+                })
+                .collect::<Vec<_>>();
+            let mut line_points = linestring_points.into_iter().flat_map(|f| f.into_iter()).collect::<Vec<_>>();
+            line_points.dedup();
+            if line_points.len() < 3 {
+                continue;
+            }
+            let line = SvgLine {
+                points: line_points,
+            };
+            if external {
+                outer_rings.push(line);
+            } else {
+                inner_rings.push(line);
+            }
+        }
 
-    let objekte = BTreeMap::new();
+        if outer_rings.is_empty() && inner_rings.is_empty() {
+            continue;
+        }
+        
+        let mut attributes = o_node.children.iter()
+        .filter_map(|cn| match &cn.text {
+            Some(s) => Some((cn.node_type.clone(), s.clone())),
+            None => None,
+        }).collect::<BTreeMap<_, _>>();
+        attributes.extend(o_node.attributes.clone().into_iter());
+
+        let tp = TaggedPolygon {
+            poly: SvgPolygon {
+                outer_rings,
+                inner_rings,
+            },
+            attributes,
+        };
+
+        objekte.insert(key, tp);
+    }
 
     NasSVGFile {
         crs: crs,
