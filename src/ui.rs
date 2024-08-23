d225b69f64db7ddc895a66f1191e0987884c722c
Felix Schütt <felix.schuett@maps4print.com>
2024-08-23 12:32:24 +0200

    Filter 0-sized splitflaeche

--- src/ui.rs
+++ src/ui.rs
@@ -1297,6 +1297,10 @@ impl AenderungenClean {
             }
         }
 
+        let is = is.into_iter().filter(|i| {
+            !i.poly_cut.is_empty() && i.poly_cut.is_zero_area()
+        }).collect::<Vec<_>>();
+
         is
 
     }
