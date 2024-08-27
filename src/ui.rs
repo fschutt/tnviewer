f13b0ec22b989b3bacde68594bc7372ca990fed5
Felix Schütt <felix.schuett@maps4print.com>
2024-08-27 04:29:09 +0200

    Disable last loop

--- src/ui.rs
+++ src/ui.rs
@@ -1413,10 +1413,12 @@ impl AenderungenClean {
             }
         }
 
+        /*
         let is = is.into_iter().filter(|i| {
             !i.poly_cut.is_empty() && !i.poly_cut.is_zero_area()
         }).collect::<Vec<_>>();
-
+        */
+        
         AenderungenIntersections(is).deduplicate().merge_to_nearest()
     }
 }
