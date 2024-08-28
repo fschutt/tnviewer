dd0776fa94fa40bc68f23b4efc566b1e92c10543
Felix Schütt <felix.schuett@maps4print.com>
2024-08-28 17:08:15 +0200

    Fix stage1 again

--- src/ui.rs
+++ src/ui.rs
@@ -1756,7 +1756,13 @@ impl Aenderungen {
             near_points.append(&mut Self::get_points_near_point(p, &poly.inner_rings, maxdst_point, maxdst_line, mode));
         }
 
-        let mut near_points = near_points.into_iter().filter(|(dst, v)| dst.abs() == 0.0).collect::<Vec<_>>();
+        let mut near_points = near_points.into_iter().filter(|(dst, v)| {
+            if v.is_line() {
+                dst.abs() > 0.001
+            } else {
+                true
+            }
+        }).collect::<Vec<_>>();
 
         if !near_points.is_empty() {
             log_1(&serde_json::to_string(&near_points).unwrap_or_default().into());
