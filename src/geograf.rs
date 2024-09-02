86c0bd6f4d5fff1d8a5e818eb38236614210594e
Felix Schütt <felix.schuett@maps4print.com>
2024-09-02 17:52:38 +0200

    Debug more

--- src/geograf.rs
+++ src/geograf.rs
@@ -698,8 +698,8 @@ pub fn get_aenderungen_nutzungsarten_linien(splitflaechen: &[AenderungenIntersec
         let b = &splitflaechen[*b];
         let mut shared_lines = get_shared_lines(&a.poly_cut, &b.poly_cut);
         if !shared_lines.is_empty() {
-            v.append(&mut shared_lines);
             log_status(&format!("NA untergehend zwischen {} ({} -> {}) and {} ({} -> {}) {} gemeinsame Linien: {shared_lines:?}", a.flst_id_part, a.alt, a.neu, b.flst_id_part, b.alt, b.neu, shared_lines.len()));    
+            v.append(&mut shared_lines);
         }
     }
 
